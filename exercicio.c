27ebd78e9aa2c482e934952d7e85db83f20986f8
AyrtonSr <133722471+AyrtonSr@users.noreply.github.com>
2023-06-05 21:40:06 -0400

    exercicio.c

--- exercicio.c
+++ exercicio.c
@@ -1,50 +1,70 @@
 #include <stdio.h>
 #include <locale.h>
 
-main(){
-setlocale(LC_ALL, "Portuguese");
-float num1, num2;
-int opcao=0;
+float num1,num2;
 
-while(opcao!=3){
-        system("CLS");
-printf("\n***********************SISTEMA DE MENUS**********************\n");
-printf("\n Op誽o 01 - SOMAR\n");
-printf("\n Op誽o 02 - SUBTRAIRo\n");
-printf("\n Op誽o 03 - SAIR DO SISTEMA\n");
-
-printf("\nDigite o nero da op誽o: ");
-scanf("%i", &opcao);
-    switch(opcao){
-    case 1:
-            printf("\nAcessando o Menu 01 - SOMAR\n");
-            printf("\n\n Digite o primeiro nero: ");
+void somar ();
+void somar (){
+printf("\n Acessando o Menu 01 - SOMAR\n");
+            printf("\n\n Digite o primeiro numero: ");
             scanf("%f", &num1);
-            printf("\n\n Digite o segundo nero: ");
+            printf("\n\n Digite o segundo numero: ");
             scanf("%f", &num2);
             printf("\n\n A soma entre %.2f + %.2f = %.2f\n\n", num1, num2, num1+num2);
             printf("\n<<<<<PRESSIONE QUALQUER TECLA PARA CONTINUAR>>>>>\n");
             printf("\n***********************************************************\n");
             getch();
-            break;
+}
 
-    case 2:
-            printf("\nAcessando o Menu 02 - SUBTRAIR\n");
-            printf("\n\n Digite o primeiro nero: ");
+void subtrair();
+void subtrair(){
+printf("\nAcessando o Menu 02 - SUBTRAIR\n");
+            printf("\n\n Digite o primeiro numero: ");
             scanf("%f", &num1);
-            printf("\n\n Digite o segundo nero: ");
+            printf("\n\n Digite o segundo numero: ");
             scanf("%f", &num2);
-            printf("\n\n A subtra誽o entre %.2f - %.2f = %.2f\n\n", num1, num2, num1-num2);
+            printf("\n\n A subtracao entre %.2f - %.2f = %.2f\n\n", num1, num2, num1-num2);
             printf("\n<<<<<PRESSIONE QUALQUER TECLA PARA CONTINUAR>>>>>\n");
             printf("\n***********************************************************\n");
             getch();
-            break;
+}
 
-   case 3:
-            printf("\nSAIR DO SISTEMA\n");
+void sair();
+void sair(){
+printf("\nSAIR DO SISTEMA\n");
             printf("\n<<<<<PRESSIONE QUALQUER TECLA PARA CONTINUAR>>>>>\n");
             printf("\n***********************************************************\n");
             getch();
+
+}
+
+
+
+int main (){
+    float num1, num2;
+    int opcao;
+    while(opcao!=3){
+        system("CLS");
+        printf("\n***********************SISTEMA DE MENUS**********************\n");
+        printf("\n Opcao 01 - SOMAR\n");
+        printf("\n Opcao 02 - SUBTRAIRo\n");
+        printf("\n Opcao 03 - SAIR DO SISTEMA\n");
+
+        printf("\nDigite o numero da opcao: ");
+        scanf("%i", &opcao);
+
+        switch(opcao){
+            case 1:
+                somar ();
+                break;
+
+            case 2:
+                subtrair();
+                break;
+
+            case 3:
+                sair ();
+
             break;
     default:
             printf("\nMenu INESISTENTE\n");
@@ -54,4 +74,5 @@ scanf("%i", &opcao);
     }
 }
     return 0;
+
 }
